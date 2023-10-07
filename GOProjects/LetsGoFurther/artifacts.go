662e71173e99aa72625bc533192d97ea845c9448
LisMorozniy <78703274+LisMorozniy@users.noreply.github.com>
2023-10-07 22:17:36 +0600

    correction

--- GOProjects/LetsGoFurther/artifacts.go
+++ GOProjects/LetsGoFurther/artifacts.go
@@ -2,20 +2,31 @@ package main
 import (
 "fmt"
 "net/http"
+"strconv"
+"github.com/julienschmidt/httprouter"
 )
-// Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
-// return a plain-text placeholder response.
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 fmt.Fprintln(w, "create a new movie")
 }
-// Add a showMovieHandler for the "GET /v1/movies/:id" endpoint. For now, we retrieve
-// the interpolated "id" parameter from the current URL and include it in a placeholder
-// response.
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	id, err := app.readIDParam(r)
-	if err != nil {
-		http.NotFound(w, r)
-		return
-	}
-fmt.Fprintf(w, "show the details of movie %d\n", id)
-}
+    id, err := app.readIDParam(r)
+    if err != nil {
+    http.NotFound(w, r)
+    return
+    }
+    movie := data.Movie{
+    ID: id,
+    CreatedAt: time.Now(),
+    Title: "Casablanca",
+    Runtime: 102,
+    Genres: []string{"drama", "romance", "war"},
+    Version: 1,
+    }
+    err = app.writeJSON(w, http.StatusOK, movie, nil)
+    if err != nil {
+    app.logger.Println(err)
+    http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+    }
+    }
+    
+    
--- GOProjects/LetsGoFurther/internal/artifacts.go
+++ GOProjects/LetsGoFurther/internal/artifacts.go
@@ -1,21 +1,16 @@
-package main
+package data
+
 import (
-"fmt"
-"net/http"
+    "time"
 )
-// Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
-// return a plain-text placeholder response.
-func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-fmt.Fprintln(w, "create a new movie")
-}
-// Add a showMovieHandler for the "GET /v1/movies/:id" endpoint. For now, we retrieve
-// the interpolated "id" parameter from the current URL and include it in a placeholder
-// response.
-func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	id, err := app.readIDParam(r)
-	if err != nil {
-		http.NotFound(w, r)
-		return
-	}
-fmt.Fprintf(w, "show the details of movie %d\n", id)
+
+type Artifact struct {
+    ID        int64     // Unique integer ID for the artifact
+    CreatedAt time.Time // Timestamp for when the artifact is added to our database
+    Name      string    // Artifact name
+    Origin    string    // Place of origin of the artifact (e.g., country, region)
+    Year      int32     // Artifact creation year
+    Type      string    // Type of artifact (e.g., clothing, tool, art)
+    Version   int32     // The version number starts at 1 and will be incremented each
+                        // time the artifact information is updated
 }
