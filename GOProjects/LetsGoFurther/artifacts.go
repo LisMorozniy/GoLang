77024174ff76391fd243fd94926742ab75274dbc
LisMorozniy <78703274+LisMorozniy@users.noreply.github.com>
2023-10-07 23:31:21 +0600

    fgt import correction

--- GOProjects/LetsGoFurther/artifacts.go
+++ GOProjects/LetsGoFurther/artifacts.go
@@ -1,12 +1,24 @@
 package main
 import (
+"encoding/json"
 "fmt"
 "net/http"
 "strconv"
 "github.com/julienschmidt/httprouter"
 )
 func (app *application) createArtifactHandler(w http.ResponseWriter, r *http.Request) {
-fmt.Fprintln(w, "create a new artifact")
+    var input struct {
+        Name string `json:"name"`
+        Year int32 `json:"year"`
+        Type string `json:"type"`
+        }
+        err := json.NewDecoder(r.Body).Decode(&input)
+        if err != nil {
+        app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+        return
+        }
+        // Dump the contents of the input struct in a HTTP response.
+        fmt.Fprintf(w, "%+v\n", input)    
 }
 func (app *application) showArtifactHandler(w http.ResponseWriter, r *http.Request) {
     id, err := app.readIDParam(r)
