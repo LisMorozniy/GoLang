dfd258a85ca4645b794c81f224b33963283137b9
LisMorozniy <78703274+LisMorozniy@users.noreply.github.com>
2023-10-19 13:24:44 +0600

    DB

--- GOProjects/LetsGoFurther/cmd/api/routes.go
+++ GOProjects/LetsGoFurther/cmd/api/routes.go
@@ -1,16 +1,19 @@
 package main
+
 import (
-"net/http"
-"github.com/julienschmidt/httprouter"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
+
 func (app *application) routes() *httprouter.Router {
-// Initialize a new httprouter router instance.
-router := httprouter.New()
-router.NotFound = http.HandlerFunc(app.notFoundResponse)
-router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
-router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-router.HandlerFunc(http.MethodPost, "/v1/artifacts", app.createArtifactHandler)
-router.HandlerFunc(http.MethodGet, "/v1/artifacts/:id", app.showArtifactHandler)
-// Return the httprouter instance.
-return router
+	// Initialize a new httprouter router instance.
+	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/artifacts", app.createArtifactHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/artifacts/:id", app.showArtifactHandler)
+	// Return the httprouter instance.
+	return router
 }
--- GOProjects/LetsGoFurther/internal/data/year.go
+++ GOProjects/LetsGoFurther/internal/data/year.go
@@ -1,17 +1,20 @@
 package data
+
 import (
-"errors"
-"fmt"
-"strconv"
-"strings"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidYearFormat = errors.New("invalid year format")
+
 type Year int32
+
 func (r Year) MarshalJSON() ([]byte, error) {
-jsonValue := fmt.Sprintf("%d BC", r)
-quotedJSONValue := strconv.Quote(jsonValue)
-return []byte(quotedJSONValue), nil
+	jsonValue := fmt.Sprintf("%d BC", r)
+	quotedJSONValue := strconv.Quote(jsonValue)
+	return []byte(quotedJSONValue), nil
 }
 func (r *Year) UnmarshalJSON(jsonValue []byte) error {
 	// We expect that the incoming JSON value will be a string in the format
@@ -20,25 +23,24 @@ func (r *Year) UnmarshalJSON(jsonValue []byte) error {
 	// ErrInvalidYearFormat error.
 	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
 	if err != nil {
-	return ErrInvalidYearFormat
+		return ErrInvalidYearFormat
 	}
 	// Split the string to isolate the part containing the number.
 	parts := strings.Split(unquotedJSONValue, " ")
 	// Sanity check the parts of the string to make sure it was in the expected format.
 	// If it isn't, we return the ErrInvalidYearFormat error again.
 	if len(parts) != 2 || parts[1] != "BC" {
-	return ErrInvalidYearFormat
+		return ErrInvalidYearFormat
 	}
 	// Otherwise, parse the string containing the number into an int32. Again, if this
 	// fails return the ErrInvalidYearFormat error.
 	i, err := strconv.ParseInt(parts[0], 10, 32)
 	if err != nil {
-	return ErrInvalidYearFormat
+		return ErrInvalidYearFormat
 	}
 	// Convert the int32 to a Year type and assign this to the receiver. Note that we
 	// use the * operator to deference the receiver (which is a pointer to a Year
 	// type) in order to set the underlying value of the pointer.
 	*r = Year(i)
 	return nil
-	}
-	
+}
